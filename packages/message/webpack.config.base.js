2a9818ebee29e9c7d654abdecf9bb3c6a194d15b
Deng Tao <d17381844023@gmail.com>
2023-03-21 19:19:59 +0800

    do pack

--- packages/message/webpack.config.base.js
+++ packages/message/webpack.config.base.js
@@ -2,7 +2,16 @@ const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const { VueLoaderPlugin } = require('vue-loader')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
 const pkgName = 'micro-message'
+rewritePkgFile(
+  {
+    main: 'src/index.js'
+  },
+  {
+    main: 'dist/micro-message.js'
+  }
+)
 const config = {
   devtool: 'source-map',
   entry: {
@@ -52,11 +61,41 @@ const config = {
       }
     ]
   },
+  externals: {
+    fs: 'fs-extra'
+  },
+  resolve: {
+    fallback: {
+      fs: false,
+      path: require.resolve('path-browserify'),
+      stream: require.resolve('stream-browserify'),
+      constants: require.resolve('constants-browserify')
+    }
+  },
   plugins: [
     new MiniCssExtractPlugin(),
     new VueLoaderPlugin(),
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+    new NodePolyfillPlugin()
   ]
 }
 
+function rewritePkgFile (devOps, prodOps) {
+  if (process.env.NODE_ENV === 'production') {
+    const fs = require('fs-extra')
+    const pkgJsonFile = JSON.parse(fs.readFileSync('./package.json'))
+    for (const key in prodOps) {
+      pkgJsonFile[key] = prodOps[key]
+    }
+    fs.writeFileSync('./package.json', JSON.stringify(pkgJsonFile))
+  }
+  if (process.env.NODE_ENV === 'development') {
+    const fs = require('fs-extra')
+    const pkgJsonFile = JSON.parse(fs.readFileSync('./package.json'))
+    for (const key in devOps) {
+      pkgJsonFile[key] = devOps[key]
+    }
+    fs.writeFileSync('./package.json', JSON.stringify(pkgJsonFile))
+  }
+}
 module.exports = config
