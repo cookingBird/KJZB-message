9b457db7e7765bf4df4caba3b1f792bd1e417472
cookingBird <86037230+cookingBird@users.noreply.github.com>
2023-12-07 00:20:22 +0800

    update a hook

--- packages/message/src/config/index.ts
+++ packages/message/src/config/index.ts
@@ -1,8 +1,15 @@
-import { SyncBailHook } from 'tapable';
+import { SyncBailHook, SyncWaterfallHook } from 'tapable';
 import { type DataMsg } from '../ApplicationChannel';
 
 export default {
   hooks: Object.freeze({
-    findRegistryEl: new SyncBailHook<DataMsg, HTMLIFrameElement>(['registerMsg'])
+    /**
+     * 当父级应用接收到子应用注册的消息时，主应用需要发现注册的应用
+     */
+    findRegistryEl: new SyncBailHook<DataMsg, HTMLIFrameElement | null>(['registerMsg']),
+    /**
+     * 当前应用bootstrap时，需要根据父应用parse出当前应用的appcode
+     */
+    praseAppCode: new SyncWaterfallHook<[string | undefined]>(),
   })
 }
